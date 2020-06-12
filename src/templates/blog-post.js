89490ccce9a86ce8028aef582a46fb189f68c900
sergiofornet <sergio.fornet@gmail.com>
2020-06-12 11:21:01 +0200

    Add path field to template.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,6 +9,7 @@ export default function BlogPost({ data }) {
 			<div>
 				<h1>{post.frontmatter.title}</h1>
 				<div dangerouslySetInnerHTML={{ __html: post.html }} />
+				<div>{post.frontmatter.path}</div>
 			</div>
 		</Layout>
 	);
@@ -20,6 +21,7 @@ export const query = graphql`
 			html
 			frontmatter {
 				title
+				path
 			}
 		}
 	}
